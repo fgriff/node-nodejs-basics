a8ca14d3c619a2b90a61023fb36fa3e65aab0ecd
fgriff <anglirum@gmail.com>
2023-06-19 01:54:47 +0400

    feat: implement env.js and args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    // Write your code here 
+  const result = process.argv
+    .slice(2)
+    .reduce((accum, _, idx, arr) => {
+      if (idx % 2 && arr[idx - 1].startsWith("--")) {
+        const formattedProp = arr[idx - 1].replace("--", "");
+        accum.push(`${formattedProp} is ${arr[idx]}`);
+      }
+
+      return accum;
+    }, [])
+    .join(", ");
+
+  console.log(result);
 };
 
 parseArgs();
