8415531469d8c55d01901fa28d5d085361b4331e
fgriff <anglirum@gmail.com>
2023-06-19 19:58:19 +0400

    feat: implement worker.js and main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,28 @@
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+
 const performCalculations = async () => {
-    // Write your code here
+  const workers = cpus().map((_, idx) => {
+    return new Promise((resolve, reject) => {
+      const __dirname = fileURLToPath(new URL('.', import.meta.url));
+      const filePath = join(__dirname, 'worker.js');
+
+      const worker = new Worker(filePath, { workerData: idx + 10 });
+
+      worker.on('message', (message) => resolve(message));
+      worker.on('error', (error) => reject(error));
+      worker.on('exit', (code) => {
+        if (code !== 0) {
+          reject(new Error(`Worker stopped. Exit code ${code}`));
+        }
+      });
+    });
+  });
+
+  const res = await Promise.all(workers);
+  console.log(res);
 };
 
 await performCalculations();
