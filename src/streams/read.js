2588f407c0bb3292326c9f6ac81b75ee81b9b4cd
fgriff <anglirum@gmail.com>
2023-06-19 14:38:27 +0400

    feat: implement streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { createReadStream } from 'fs';
+import { stdout } from 'process';
+import { EOL } from 'os';
+
 const read = async () => {
-    // Write your code here 
+  const __dirname = fileURLToPath(new URL('.', import.meta.url));
+  const filePath = join(__dirname, 'files', 'fileToRead.txt');
+
+  const rs = createReadStream(filePath, { encoding: 'utf8' });
+  rs.on('data', (chunk) => stdout.write(`${chunk}${EOL}`));
 };
 
 await read();
