d78416453ca68c0688add7d1358a70c11a898883
fgriff <anglirum@gmail.com>
2023-06-19 15:47:06 +0400

    feat: implement streams/transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,24 @@
+import { Transform } from 'stream';
+import { stdin, stdout } from 'process';
+import { EOL } from 'os';
+
 const transform = async () => {
-    // Write your code here 
+  const strTransform = new Transform({
+    transform(chunk, _, callback) {
+      const reversedStr = chunk
+        .toString('utf8')
+        .replace(EOL, '')
+        .split('')
+        .reverse()
+        .join('') + EOL;
+
+      this.push(reversedStr);
+      callback();
+    },
+  });
+
+  stdout.write(`Enter some text (press CTRL+C to exit):${EOL}`);
+  stdin.pipe(strTransform).pipe(stdout);
 };
 
 await transform();
