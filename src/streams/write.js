96041bff9e2879a7a304068dbc8661189566ed9f
fgriff <anglirum@gmail.com>
2023-06-19 15:06:11 +0400

    feat: implement streams/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,35 @@
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { createWriteStream } from 'fs';
+import { stdin, stdout } from 'process';
+import { createInterface } from 'readline';
+import { EOL } from 'os';
+
 const write = async () => {
-    // Write your code here 
+  const __dirname = fileURLToPath(new URL('.', import.meta.url));
+  const filePath = join(__dirname, 'files', 'fileToWrite.txt');
+
+  const ws = createWriteStream(filePath, { encoding: 'utf8' });
+
+  const rl = createInterface({
+    input: stdin,
+    output: stdout,
+  });
+
+  const exit = () => {
+    rl.close();
+    ws.end();
+  };
+
+  stdout.write(`Enter some text (press CTRL+C to exit):${EOL}`);
+
+  rl.on('line', (text) => {
+    ws.write(`${text}${EOL}`);
+  });
+
+  rl.on('SIGINT', () => {
+    exit();
+  });
 };
 
 await write();
